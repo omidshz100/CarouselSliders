c1aac2b86ed5f2904d8400be2ab6927497bc9be4
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2023-11-10 16:33:44 +0100

    Initial Commit

---
+++ CarouselSliders/CarouselSlidersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselSlidersApp.swift
+//  CarouselSliders
+//
+//  Created by Omid Shojaeian Zanjani on 10/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CarouselSlidersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
